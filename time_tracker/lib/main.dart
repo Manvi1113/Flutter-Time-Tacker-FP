7603e21ee4bbaef6803b98d458de34178c1d9959
ritikaj23 <ritikaj@skillup.online>
2024-10-11 08:09:19 -0400

    initial

--- time_tracker/lib/main.dart
+++ time_tracker/lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:provider/provider.dart';
-import 'providers/project_task_provider.dart';
-import 'providers/time_entry_provider.dart';
-import 'screens/add_time_entry_screen.dart';
+
+import 'providers/project_task_provider.dart'; // Adjusting to include your provider
+import 'providers/time_entry_provider.dart'; // Adjusting to include your provider
+import 'screens/add_time_entry_screen.dart'; // Adjusting to include your screens
 import 'screens/home_screen.dart';
 import 'screens/project_task_management_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Initialize LocalStorage
-  final LocalStorage localStorage = LocalStorage('my_app'); // Ensure you have imported this correctly
-  await localStorage.ready; // Wait until local storage is ready
-
+  await initLocalStorage();
   runApp(MyApp(localStorage: localStorage));
 }
 
@@ -26,16 +23,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => TimeEntryProvider(localStorage)),
         ChangeNotifierProvider(create: (_) => ProjectTaskProvider(localStorage)),
+        ChangeNotifierProvider(create: (_) => TimeEntryProvider(localStorage)),
       ],
       child: MaterialApp(
-        title: 'Time Tracker',
+        title: 'Time Tracker', // Change the title to match your app
         debugShowCheckedModeBanner: false,
         initialRoute: '/',
         routes: {
-          '/': (context) => HomeScreen(),
-          '/Manage_Projects_Tasks': (context) => ProjectTaskManagementScreen(),
+          '/': (context) => HomeScreen(), // Main entry point, HomeScreen
+          '/manage_projects_tasks': (context) => ProjectTaskManagementScreen(), // Route for managing projects and tasks
+          '/add_time_entry': (context) => AddTimeEntryScreen(), // Route for adding time entries
         },
       ),
     );
